b1e04e84bdec5fc8abf2e805aa427e6d26829106
Shrey0610 <shreyshah241@gmail.com>
2024-02-25 08:27:20 +0530

    pushed

--- LeerMeester/src/App.jsx
+++ LeerMeester/src/App.jsx
@@ -1,33 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+     
+  <div className="container-fluid">
+  <a className="navbar-brand" href="#">
+            <img className="logo" 
+              src="/LeerMeester.png"
+              alt="Bootstrap"
+              width={60}
+              height={60}
+            />
+          </a>
+  </div>
+    <div className="navbar navbar-collapse-lg" id="navbarNav">
+      <center />
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Features
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Pricing
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">
+            Disabled
+          </a>
+        </li>
+      </ul>
+
+    </div>
+
+</nav>
+
     </>
   )
 }
